0735026f1894a61da35c596e4e1412f19d22e1d5
Amir <amirhamzan30@gmail.com>
2022-09-30 15:41:08 +0800

    Add primevue package, style component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
 import './bootstrap';
 
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 
 import App from './vue/App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount("#app")
+import 'primevue/resources/themes/lara-light-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
+import ToastService from 'primevue/toastservice';
+
+createApp(App).use(PrimeVue).use(ToastService).mount("#app")
--- resources/js/vue/App.vue
+++ resources/js/vue/App.vue
@@ -1,10 +1,54 @@
 <template>
-    <div>this is app.vue</div>
+  <div class="grid mt-8">
+    <div class="col-6 col-offset-3">
+      <Card class="test">
+        <template #title> Add Todo List <i class="pi pi-plus"></i></template>
+        <template #content>
+          <div class="field">
+            <label for="firstname1">Fill up your routine!</label>
+            <input
+              id="firstname1"
+              type="text"
+              class="
+                text-base text-color
+                surface-overlay
+                p-2
+                border-1 border-solid
+                surface-border
+                border-round
+                appearance-none
+                outline-none
+                focus:border-primary
+                w-full
+              "
+            />
+          </div>
+          <Button label="Submit" />
+        </template>
+      </Card>
+      <Card class="mt-2">
+        <template #title> Recorded List <i class="pi pi-check"></i></template>
+      </Card>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
-</script>
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+import Card from "primevue/card";
+import Dialog from "primevue/dialog";
 
+export default {
+  components: {
+    InputText,
+    Button,
+    Card,
+    Dialog,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
